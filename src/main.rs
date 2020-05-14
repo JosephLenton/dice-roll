7559647e9d01d66150df3dd8c9ca0906e4b1be22
joseph.lenton@askattest.com <joseph.lenton@askattest.com>
2020-05-14 05:19:03 +0100

    remove the string from formatting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![feature(box_syntax, box_patterns)]
 
 use ::std::fmt;
-use ::std::fmt::{Write};
 use ::std::env;
 use ::pom;
 
@@ -10,10 +9,11 @@ mod parse;
 mod eval;
 
 fn main() -> Result<(), fmt::Error> {
+    let name = "You";
     let input = env::args().skip(1).collect::<Vec<String>>().join(" ");
     let result = run(&input);
-    let output = format_result(&"you", &input, result)?;
-    println!("{}", output);
+
+    println!("{} rolled ... {}\nThey got ... {}", name, input, ResultOutput(result));
 
     Ok(())
 }
@@ -26,59 +26,52 @@ fn run(input:&str) -> Result<Vec<Result<eval::Output, eval::Error>>, pom::Error>
     Ok(eval_results)
 }
 
-fn format_result(
-    name: &str,
-    input: &str,
-    result: Result<Vec<Result<eval::Output, eval::Error>>, pom::Error>,
-) -> Result<String, fmt::Error> {
-    let mut msg = String::new();
+struct ResultOutput(Result<Vec<Result<eval::Output, eval::Error>>, pom::Error>);
 
-    writeln!(&mut msg, "{} rolled ... {}", name, input)?;
-    write!(&mut msg, "They got ... ")?;
-    match result {
-        Ok(outputs) => format_parse_ok(&mut msg, outputs),
-        Err(parse_err) => format_parse_err(&mut msg, &parse_err),
-    }?;
-    writeln!(&mut msg, "")?;
-
-    Ok(msg)
+impl fmt::Display for ResultOutput {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ResultOutput(Ok(outputs)) => format_parse_ok(f, &outputs),
+            ResultOutput(Err(parse_err)) => format_parse_err(f, &parse_err),
+        }
+    }
 }
 
-fn format_parse_ok(msg:&mut String, outputs: Vec<Result<eval::Output, eval::Error>>) -> fmt::Result {
-    for (i, output) in outputs.into_iter().enumerate() {
+fn format_parse_ok(f:&mut fmt::Formatter<'_>, outputs: &Vec<Result<eval::Output, eval::Error>>) -> fmt::Result {
+    for (i, output) in outputs.iter().enumerate() {
         if i > 0 {
-            write!(msg, " ")?;
+            write!(f, " ")?;
         }
 
-        format_output(msg, output)?;
+        format_output(f, output)?;
     }
 
     Ok(())
 }
 
-fn format_parse_err(msg: &mut String, err: &pom::Error) -> fmt::Result {
-    write!(msg, "error ... {:?}", err)
+fn format_parse_err(f: &mut fmt::Formatter<'_>, err: &pom::Error) -> fmt::Result {
+    write!(f, "error ... {:?}", err)
 }
 
-fn format_output(msg:&mut String, output_result: Result<eval::Output, eval::Error>) -> fmt::Result {
+fn format_output(f:&mut fmt::Formatter<'_>, output_result: &Result<eval::Output, eval::Error>) -> fmt::Result {
     match output_result {
-        Ok(output) => format_output_ok(msg, output),
-        Err(err) => format_output_err(msg, err),
+        Ok(output) => format_output_ok(f, output),
+        Err(err) => format_output_err(f, err),
     }
 }
 
-fn format_output_ok(msg: &mut String, output:eval::Output) -> fmt::Result {
-    write!(msg, "{}", output)
+fn format_output_ok(f: &mut fmt::Formatter<'_>, output:&eval::Output) -> fmt::Result {
+    write!(f, "{}", output)
 }
 
-fn format_output_err(msg: &mut String, err:eval::Error) -> fmt::Result {
-    write!(msg, "{}", err)
+fn format_output_err(f: &mut fmt::Formatter<'_>, err:&eval::Error) -> fmt::Result {
+    write!(f, "{}", err)
 }
 
 impl fmt::Display for eval::Output {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-        Self::Integer(n) => write!(f, "{}", n),
+            Self::Integer(n) => write!(f, "{}", n),
         }
     }
 }
