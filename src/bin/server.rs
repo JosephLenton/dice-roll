b2ede7ef0069ee78e12de2af758044afee39cccc
joseph.lenton@askattest.com <joseph.lenton@askattest.com>
2020-05-15 02:44:12 +0100

    add binding to other ports

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -2,12 +2,18 @@
 
 use ::actix_web::{get, web, App, HttpServer, Responder};
 use ::dice_roll;
+use ::std::env;
 use ::std::io;
+use std::net::{SocketAddr, ToSocketAddrs};
+
+const DEFAULT_PORT: u16 = 80;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
+    let address = get_address();
+    println!("Running on ... {}", address);
     HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:8080")?
+        .bind(address)?
         .run()
         .await
 }
@@ -20,4 +26,22 @@ async fn index(info: web::Path<String>) -> impl Responder {
     dice_roll::main(&username, &info, &mut response);
 
     String::from_utf8(response).unwrap()
+}
+
+fn get_address() -> SocketAddr {
+    SocketAddr::from(([0, 0, 0, 0], get_port()))
+        .to_socket_addrs()
+        .unwrap()
+        .next()
+        .unwrap()
+}
+
+fn get_port() -> u16 {
+    match env::var("PORT") {
+        Ok(port) => port.parse::<u16>().unwrap(),
+        Err(_) => {
+            println!("Couldn't find port, using ... {}", DEFAULT_PORT);
+            DEFAULT_PORT
+        }
+    }
 }
